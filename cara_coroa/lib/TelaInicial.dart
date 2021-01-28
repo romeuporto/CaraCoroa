c9a32740f7f67b326a4388db77a76ece865da281
Romeu Porto <romeu.porto.sms@gmail.com>
2021-01-27 23:04:36 -0300

    Tela Inicial ok!

--- cara_coroa/lib/TelaInicial.dart
+++ cara_coroa/lib/TelaInicial.dart
@@ -6,11 +6,25 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+
+  void _exibirResultado(){}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Color(0xff61bd86),
       body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("images/botao_jogar.png"),
+            )
+          ],
+        ),
 
       ),
     );
