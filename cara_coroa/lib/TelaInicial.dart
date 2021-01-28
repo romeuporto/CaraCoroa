74c245a30952f844f0a5c7459f14641669916183
Romeu Porto <romeu.porto.sms@gmail.com>
2021-01-27 22:51:09 -0300

    Criate app

---
+++ cara_coroa/lib/TelaInicial.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatefulWidget {
+  @override
+  _GameState createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Container(
+
+      ),
+    );
+  }
+}
---
+++ cara_coroa/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'TelaInicial.dart';
+
+void main(){
+  runApp(MaterialApp(
+  home: Game(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
